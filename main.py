80d3871de2cdf14d0183cdce1cdec88aabd0e7f1
Lee Korff-Korn <leekk270@gmail.com>
2020-12-27 05:43:21 -0500

    added golden_cross func

--- main.py
+++ main.py
@@ -45,3 +45,54 @@ def limit_btc_sell(current_price):
     order_id = limit_order_info['id']
     print('limit order TRY SELL {} status: {}'.format(order_id, r.order_status(order_id)))
     # print('canceling limit SELL order {}: {}'.format(order_id, r.order_cancel(order_id)))
+
+
+def golden_cross(current_price, history, stockTicker, n1, n2, days, direction=""):
+    # if(direction == "above" and not five_year_check(stockTicker)):
+    #     return False
+    # history = r.get_historicals(stockTicker,span='year',bounds='regular')
+    closingPrices = []
+    dates = []
+    total_price = 0
+    count  = 0
+    for item in history:
+        # print(item)
+        closingPrices.append(float(item['close_price']))
+        dates.append(item['begins_at'])
+        print(float(item['close_price']), item['begins_at'])
+        total_price += float(item['close_price'])
+        count+=1
+    print('currenct_price: ', current_price)
+    print('total_price: ', total_price)
+    print('count: ', count)
+    ema = total_price/count
+    print('ema: ', ema)
+
+    if current_price > ema:
+        return 'buy'
+    else:
+        return 'sell'
+
+    # price = pd.Series(closingPrices)
+    # dates = pd.Series(dates)
+    # dates = pd.to_datetime(dates)
+    # sma1 = ta.volatility.bollinger_mavg(price, n1, fillna=False)
+    # sma2 = ta.volatility.bollinger_mavg(price, n2, fillna=False)
+    # series = [price.rename("Price"), sma1.rename("Indicator1"), sma2.rename("Indicator2"), dates.rename("Dates")]
+    # df = pd.concat(series, axis=1)
+    # cross = get_last_crossing(df, days, symbol=stockTicker, direction=direction)
+    # if(cross) and plot:
+    #     show_plot(price, sma1, sma2, dates, symbol=stockTicker, label1=str(n1)+" day SMA", label2=str(n2)+" day SMA")
+    # return cross
+
+def sell_holdings(symbol, holdings_data):
+    """ Place an order to sell all holdings of a stock.
+
+    Args:
+        symbol(str): Symbol of the stock we want to sell
+        holdings_data(dict): dict obtained from get_modified_holdings() method
+    """
+    shares_owned = int(float(holdings_data[symbol].get("quantity")))
+    if not debug:
+        r.order_sell_market(symbol, shares_owned)
+    print("####### Selling " + str(shares_owned) + " shares of " + symbol + " #######")
